592b6834b6f9e317d7fc600bbffe9c306a598240
Eneida Lame <Eneida_lame@hotmail.com>
2022-05-19 20:30:12 +0300

    options

--- Tweetornyc/Tweetornyc/Program.cs
+++ Tweetornyc/Tweetornyc/Program.cs
@@ -10,6 +10,7 @@ namespace Tweetornyc
     {
         static async Task Main(string[] args)
         {
+            OptionMenu();
             var appCredentials = new ConsumerOnlyCredentials("yjqoED31on4ZQL6VXtLI7BpX6", "fPRgFnxVqLV6znax3IvjjIVdfAodB5guGyM73AWd3KTxP4ZJ3Y")
             {
                 BearerToken = "AAAAAAAAAAAAAAAAAAAAALfAagEAAAAArq15M%2FS9GBjFELBHHRxAXbdfJEE%3DK7kABEuy5wrIeYhRYgVw9JBbOSosOPnGbofehszSjuW9aKyqPk"
@@ -23,49 +24,73 @@ namespace Tweetornyc
             await Getstream(userClient);
 
             await Gettweet(userClient);
+
+            
+            
         }
 
+        public static void OptionMenu()
+        {
+            Console.WriteLine("1.Get Stream");
+            Console.WriteLine("2.Get Tweet");
+            Console.WriteLine("Plese select an option: ");
+            
+        }
+        public static string InputString()
+        {
+            return Console.ReadLine();
+        }
         static async Task Getstream(TwitterClient userClient)
         {
-            Console.WriteLine("Enter Key Word:");
-            string KeyWord = Console.ReadLine();
-            var stream = userClient.Streams.CreateFilteredStream();
-
-            stream.AddTrack(KeyWord);
-
-            List<string> tweets = new List<string>();
-
-            int i = 0;
-            stream.MatchingTweetReceived += (sender, eventReceived) =>
+            string choice = InputString();
+            if (choice == "1")
             {
+                
+                Console.WriteLine("Enter Key Word:");
+                string KeyWord = Console.ReadLine();
+                var stream = userClient.Streams.CreateFilteredStream();
+
+                stream.AddTrack(KeyWord);
+
+                List<string> tweets = new List<string>();
+
+                int i = 0;
+                stream.MatchingTweetReceived += (sender, eventReceived) =>
+                {
                 //Console.WriteLine(eventReceived.Tweet);
                 tweets.Add(eventReceived.Tweet.ToString());
-                if (i == 20)
+                    if (i == 20)
+                    {
+                        stream.Stop();
+                        Console.WriteLine("Complete!");
+                    }
+
+                    ++i;
+                };
+
+                await stream.StartMatchingAnyConditionAsync();
+
+
+                foreach (var t in tweets)
                 {
-                    stream.Stop();
-                    Console.WriteLine("Complete!");
+                    Console.WriteLine("Tweet: " + t);
                 }
 
-                ++i;
-            };
-
-            await stream.StartMatchingAnyConditionAsync();
-
-
-            foreach (var t in tweets)
-            {
-                Console.WriteLine("Tweet: " + t);
+                Console.WriteLine("Total tweets: " + tweets.Count);
             }
-
-            Console.WriteLine("Total tweets: " + tweets.Count);
         }
 
         static async Task Gettweet(TwitterClient userClient)
         {
-            var tweets = await userClient.Search.SearchTweetsAsync("2022");
-            foreach (var x in tweets)
+
+            string choice = InputString();
+            if (choice == "2")
             {
-                Console.WriteLine("Tweet: "+x);
+                var tweets = await userClient.Search.SearchTweetsAsync("2022");
+                foreach (var x in tweets)
+                {
+                    Console.WriteLine("Tweet: " + x);
+                }
             }
         }
     }
